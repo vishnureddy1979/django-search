bb4876a07ef280272450f27ecfdc68435363ac49
Vishnu <vishnu.reddy28@gmail.com>
2021-11-12 17:14:01 -0500

    First commit

---
+++ searchapp/management/commands/populate.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+from faker import Faker
+from searchapp.models import Product
+from random import randrange
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        faker = Faker()
+
+        for _ in range(50):
+            Product.objects.create(
+                title=faker.name(),
+                description = faker.text(200),
+                image=faker.image_url(),
+                price = randrange(10, 100)
+            )
